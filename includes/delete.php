c9d4f59690b268d54a3923691d2d49f4379b80c4
Noajuan <54243230+Noajuan@users.noreply.github.com>
2020-03-17 00:11:50 -0500

    Queda funcionando delete bien

--- includes/delete.php
+++ includes/delete.php
@@ -1,19 +1,18 @@
 <?php
     include("connect.php");
-
-    if(isset($_GET['delete'])){
-        if(isset($_GET["username"])){   
-            $nick = trim($_GET['username']);
-            $query = "DELETE FROM `info_user` WHERE `info_user`.`nick` = '$nick' ";
-            $rows = mysqli_query($conexion, $query);
-            if ($rows){
-                    $_SESSION['message'] = 'Se han borrado los datos';
-                    $_SESSION['message_type'] = 'danger';
-            }else{
-                    $_SESSION['message'] = 'Verifique el Nickname';
-                    $_SESSION['message_type'] = 'warning';
+    if(isset($_GET["username"])){   
+        $nick = trim($_GET['username']);
+        $query = "SELECT * FROM `info_user` WHERE `info_user`.`nick` = '$nick' ";
+        $rows = mysqli_query($conexion, $query);
+        if ($row=mysqli_fetch_array($rows)){
+            $delete = "DELETE FROM `info_user` WHERE `info_user`.`nick` = '$nick' ";
+            $del = mysqli_query($conexion, $delete);
+            $_SESSION['message'] = 'Se han eliminado los datos';
+            $_SESSION['message_type'] = 'danger';
+        }else{
+            $_SESSION['message'] = 'Verifique el usuario';
+            $_SESSION['message_type'] = 'warning';
             }
             header("Location: http://localhost/dashboard/p/public/index.php");
         }
-    }   
 ?>
